cdddafcd5a336692bc1faf0681b162b35d25d362
Dorayne McKern <dorayne@gmail.com>
2016-02-08 18:49:15 -0800

    Added function area_tri to calculate the area of the given triangle

--- Triangle.py
+++ Triangle.py
@@ -9,6 +9,12 @@ def error_check(test):
         print "Invalid input, please try again and enter a whole number"
         exit()
 
+def area_tri(x, y, z):
+    global area
+    perim = (x + y + z) / 2
+    area = math.sqrt(perim * (perim - x) * (perim - y) * (perim - z))
+    return area
+
 a = raw_input("Enter the length of first side of a triangle \n")
 if len(a) > 0:
     side_a = error_check(a)
@@ -19,4 +25,7 @@ c = raw_input("Enter the length of third side of a triangle \n")
 if len(c) > 0:
     side_c = error_check(c)
 
-print "The lengths of the sides of the triangle are %s, %s, %s" % a, b, c
+area_tri(side_a, side_b, side_c)
+
+print "The lengths of the sides of the triangle are %d, %d, %d" % (side_a, side_b, side_c)
+print "The area of the triangle with the given sides is %d" % (area)
